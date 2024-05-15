4e383c7d78ce2de0205d4fa7589cf900445c54c2
amrito502 <amritobosu7@gmail.com>
2024-05-15 12:26:54 +0600

    React Dashboard Templating

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./components/partials/Nav";
+import "./assets/css/styles.css";
+import "./assets/js/scripts";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import SideBar from "./components/partials/SideBar";
+import Footer from "./components/partials/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="sb-nav-fixed">
+      <Nav />
+      <div id="layoutSidenav">
+        <SideBar />
+        <div id="layoutSidenav_content">
+          <main>
+            <div class="container-fluid px-4">
+              <h1 class="mt-4">Dashboard</h1>
+              <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item active">Dashboard</li>
+              </ol>
+              <div class="row">
+                <div class="col-xl-3 col-md-6">
+                  <div class="card bg-primary text-white mb-4">
+                    <div class="card-body">Primary Card</div>
+                    <div class="card-footer d-flex align-items-center justify-content-between">
+                      <a class="small text-white stretched-link" href="#">
+                        View Details
+                      </a>
+                      <div class="small text-white">
+                        <i class="fas fa-angle-right"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-xl-3 col-md-6">
+                  <div class="card bg-warning text-white mb-4">
+                    <div class="card-body">Warning Card</div>
+                    <div class="card-footer d-flex align-items-center justify-content-between">
+                      <a class="small text-white stretched-link" href="#">
+                        View Details
+                      </a>
+                      <div class="small text-white">
+                        <i class="fas fa-angle-right"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-xl-3 col-md-6">
+                  <div class="card bg-success text-white mb-4">
+                    <div class="card-body">Success Card</div>
+                    <div class="card-footer d-flex align-items-center justify-content-between">
+                      <a class="small text-white stretched-link" href="#">
+                        View Details
+                      </a>
+                      <div class="small text-white">
+                        <i class="fas fa-angle-right"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-xl-3 col-md-6">
+                  <div class="card bg-danger text-white mb-4">
+                    <div class="card-body">Danger Card</div>
+                    <div class="card-footer d-flex align-items-center justify-content-between">
+                      <a class="small text-white stretched-link" href="#">
+                        View Details
+                      </a>
+                      <div class="small text-white">
+                        <i class="fas fa-angle-right"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </main>
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 }
